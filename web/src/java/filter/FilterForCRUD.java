19aeff9fdc034366c1ad8ea7f3953bd53816de36
phamkhiem004 <tula5904@gmail.com>
2025-02-13 22:23:37 +0700

    fix bug2

--- web/src/java/filter/FilterForCRUD.java
+++ web/src/java/filter/FilterForCRUD.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
  */
@@ -34,8 +34,7 @@ public class FilterForCRUD implements Filter {
     // configured. 
     private FilterConfig filterConfig = null;
 
-    public FilterForCRUD() {
-    }
+    
 
     private void doBeforeProcessing(ServletRequest request, ServletResponse response)
             throws IOException, ServletException {
