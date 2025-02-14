aefb6855ee7f4cca5cc3fea664c56baa10b7a1a7
lqbao12 <baolqhe181451@fpt.edu.vn>
2025-02-14 07:42:50 +0700

    fix bug 8

--- web/src/java/controller/SearchServlet.java
+++ web/src/java/controller/SearchServlet.java
@@ -29,6 +29,7 @@ public class SearchServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // Method is not implemented yet as POST request handling is not needed for this feature
 
     }
 
